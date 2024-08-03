06654fc454ea18c2666bcaa524f32d8ad625a84d
Jason Newsom <jnewsom405@gmail.com>
2024-08-03 10:28:34 -0600

    made some html & css changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,7 @@
+import "./styles.css";
+// import BootStrapClient from "../components/BootstrapClient";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+  <BootStrapClient />;
 }
