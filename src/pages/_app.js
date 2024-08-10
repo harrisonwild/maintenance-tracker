714e86344a0c6c943d0f17718cc7859f1ee34c49
Jason Newsom <jnewsom405@gmail.com>
2024-08-10 11:16:06 -0500

    this is the correct code for working Boostrap and updated form

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,20 @@
-import "./styles.css";
-// import BootStrapClient from "../components/BootstrapClient";
+// import "./styles.css";
+// // import BootStrapClient from "../components/BootstrapClient";
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+//   <BootStrapClient />;
+// }
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }
+  }, []);
+
   return <Component {...pageProps} />;
-  <BootStrapClient />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,3 @@
-// export default function Home() {
-//   return (
-//     <>
-//       <h1>Dispenser Repair Form</h1>
-//       <div class="whole-form">
-//         <div class="customer-info">
-//           <form action="" method="get" class="dispenser-form">
-//             <div class="dispenser-form">
-//               <label for="account-name">Account Name: </label>
-//               <input
-//                 type="text"
-//                 name="account-name"
-//                 id="account-name"
-//                 required
-//               />
-//             </div>
-//             <div class="dispenser-form">
-//               <label for="contact-name">Contact Name: </label>
-//               <input
-//                 type="text"
-//                 name="contact-name"
-//                 id="contact-name"
-//                 required
-//               />
-//             </div>
-//             <div class="dispenser-form">
-//               <label for="address">Account Address: </label>
-//               <input type="text" name="address" id="address" required />
-//             </div>
-//             <div class="dispenser-form">
-//               <label for="special">Special Instructions </label>
-//               <input type="text" name="instructions" id="instructions" />
-//             </div>
-//             <div class="dispenser-form">
-//               <label for="hours">Hours of Operation </label>
-//               <input type="text" name="hours" id="hours" required />
-//             </div>
-//           </form>
-//           <form>
-//             <div class="dispenser-info">
-//               <div class="dispenser-form">
-//                 <label for="brand">Dispenser Brand </label>
-//                 <input type="text" name="brand" id="brand" required />
-//               </div>
-//               <div class="dispenser-form">
-//                 <label for="dispenser-type">Dispenser Type </label>
-//                 <input type="text" name="dispenser-type" id="dispenser-type" />
-//               </div>
-//               <div class="dispenser-form">
-//                 <label for="colors">Color (SIG only)</label>
-//                 <input
-//                   type="text"
-//                   name="dispenser-color"
-//                   id="dispenser-color"
-//                 />
-//               </div>
-//               <div class="dispenser-form">
-//                 <label for="dispenser-qty">Quantity </label>
-//                 <input type="text" name="quantity" id="quantity" />
-//               </div>
-//               <div class="dispenser-form">
-//                 <label for="problem">Problem Found </label>
-//                 <input type="text" name="problem" id="problem" />
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// ----------------------
-
 export default function Home() {
   return (
     <>
@@ -89,29 +15,33 @@ export default function Home() {
                       <div class="row">
                         <div class="col-md-6">
                           <div class="form-group">
-                            <label for="form_name">Firstname *</label>
+                            <label for="form_account_name">
+                              Account name *
+                            </label>
                             <input
                               id="form_name"
                               type="text"
-                              name="name"
+                              name="account-name"
                               class="form-control"
-                              placeholder="Please enter your firstname *"
+                              placeholder="Please enter the account name *"
                               required="required"
-                              data-error="Firstname is required."
+                              data-error="Account name is required."
                             ></input>
                           </div>
                         </div>
                         <div class="col-md-6">
                           <div class="form-group">
-                            <label for="form_lastname">Lastname *</label>
+                            <label for="form_contact_name">
+                              Contact's name *
+                            </label>
                             <input
-                              id="form_lastname"
+                              id="form_contact_name"
                               type="text"
-                              name="surname"
+                              name="contact-name"
                               class="form-control"
-                              placeholder="Please enter your lastname *"
+                              placeholder="Please enter the contact's full name *"
                               required="required"
-                              data-error="Lastname is required."
+                              data-error="Contact's name is required."
                             ></input>
                           </div>
                         </div>
@@ -119,15 +49,33 @@ export default function Home() {
                       <div class="row">
                         <div class="col-md-6">
                           <div class="form-group">
-                            <label for="form_email">Email *</label>
+                            <label for="account_address">
+                              Account's Address *
+                            </label>
                             <input
-                              id="form_email"
-                              type="email"
-                              name="email"
+                              id="form_address"
+                              type="text"
+                              name="account-address"
                               class="form-control"
-                              placeholder="Please enter your email *"
+                              placeholder="Please enter the address *"
                               required="required"
-                              data-error="Valid email is required."
+                              data-error="Account's address is required."
+                            ></input>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="account_hours">
+                              Account's Hours of Operations *
+                            </label>
+                            <input
+                              id="form_hours"
+                              type="text"
+                              name="account-hours"
+                              class="form-control"
+                              placeholder="Please enter the hours of operation *"
+                              required="required"
+                              data-error="Account's hours of operation is required."
                             ></input>
                           </div>
                         </div>
@@ -146,10 +94,12 @@ export default function Home() {
                               <option value="" selected disabled>
                                 --Select Your Issue--
                               </option>
-                              <option>Request Invoice for order</option>
-                              <option>Request order status</option>
-                              <option>Haven't received cashback yet</option>
-                              <option>Other</option>
+                              <option>New Dispenser Install</option>
+                              <option>Repair/Replace Existing Dispenser</option>
+                              <option></option>
+                              <option>
+                                Other: Leave detailed message below
+                              </option>
                             </select>
                           </div>
                         </div>
@@ -162,7 +112,7 @@ export default function Home() {
                               id="form_message"
                               name="message"
                               class="form-control"
-                              placeholder="Write your message here."
+                              placeholder="Write helpful instructions here."
                               rows="4"
                               required="required"
                               data-error="Please, leave us a message."
@@ -175,7 +125,8 @@ export default function Home() {
                             type="submit"
                             class="btn btn-success btn-send  pt-2 btn-block
                             "
-                            value="Send Message"
+                            value="Submit Form"
+                            style={{ padding: "10px", marginTop: "10px" }}
                           ></input>
                         </div>
                       </div>
